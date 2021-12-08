ba632310710cf204d709656c1cb525bfdba43c53
Thiago Oliveira <mush@vista.aero>
2021-12-07 21:23:16 -0300

    Initial Commit

---
+++ TheDogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheDogs
+//
+//  Created by Thiago Oliveira on 07/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
